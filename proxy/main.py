9b643e80b4ed2d315e0e90aaa7c1d77eed1f3b5c
Derek Michael Frank <derekmfrank@gmail.com>
2017-05-04 08:37:20 -0700

    feat(proxy): Make server asynchronous
    * Use gevent (greenlets) server

--- proxy/main.py
+++ proxy/main.py
@@ -2,6 +2,12 @@
 # IMPORTS
 ################################################################################
 
+# Important preconfiguration.
+from gevent import monkey; monkey.patch_all()
+
+# Stdlib.
+from time import sleep
+
 # Related 3rd party.
 from bottle import route, run
 
@@ -15,9 +21,15 @@ def hello():
     return 'Hello, world!'
 
 
+@route('/foo')
+def foobar():
+    sleep(20)
+    return 'foobar for 20 seconds!'
+
+
 ################################################################################
 # MAIN
 ################################################################################
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080, debug=True)
+    run(server='gevent', host='0.0.0.0', port=8080, debug=True)
